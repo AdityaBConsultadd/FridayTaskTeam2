af2804d276ef83860ae929c59ded15edf686bd04
priyak4790 <priyak4790@gmail.com>
2021-03-05 15:10:47 -0600

    Adding class from priyanka

---
+++ com/task/Priyanka.java
@@ -0,0 +1,22 @@
+package com.task;
+
+public class Priyanka {
+
+	public static void main(String args[])
+	{
+	
+				// TODO Auto-generated method stub
+				String s1 ="123";
+				String s2 = "122";
+				int i,j;
+				
+			 i = Integer.parseInt(s1);
+				j = Integer.parseInt(s2);
+				
+				System.out.println("The sum of numbers is :" +(i+j));
+			}
+
+		}
+
+	
+
