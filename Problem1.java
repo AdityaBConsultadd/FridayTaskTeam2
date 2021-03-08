46d07cebd6c3f3f8c2a8e80fa9459f47e347fdb3
priyak4790 <priyak4790@gmail.com>
2021-03-07 23:30:33 -0600

    Adding prob1 and prob2

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,25 @@
+public class Problem1 {
+
+	public static void main(String args[])
+	{
+		
+	Priyanka p = new Priyanka();
+	p.add();
+	}
+}
+	class Priyanka
+	{
+		void add() {
+				// TODO Auto-generated method stub
+				String s1 ="123";
+				String s2 = "122";
+				int i,j;
+				
+			 i = Integer.parseInt(s1);
+				j = Integer.parseInt(s2);
+				
+				System.out.println("The sum of numbers is :" +(i+j));
+			}
+
+		}
 
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,44 @@
 
+public class Problem2 {
+
+	public static void main(String args[])
+	{
+		
+	Priyanka1 p = new Priyanka1();
+	p.abc();
+	}
+}
+	class Priyanka1
+	{
+		void abc() {
+
+		char arr [] = {'a','a','a','b','b','b','c','c','c','c'};
+		int[] freq = new int[arr.length];  
+        int i, j;  
+           
+          
+        for(i = 0; i <arr.length; i++) {  
+            freq[i] = 1;  
+            for(j = i+1; j <arr.length; j++) {  
+                if(arr[i] == arr[j]) {  
+                    freq[i]++;  
+                      
+                    //Set string[j] to 0 to avoid printing visited character  
+                    arr[j] = '0';  
+                }  
+            }  
+        }  
+          
+        //Displays the each character and their corresponding frequency  
+        System.out.println("Characters and their corresponding frequencies");  
+        for(i = 0; i <freq.length; i++) {  
+            if(arr[i] != ' ' && arr[i] != '0')  
+                System.out.println(arr[i] + "-" + freq[i]);  
+        }  
+    }  
+ 
+		
+		
+		
+		}
+	
